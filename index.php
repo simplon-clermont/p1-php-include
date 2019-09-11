9d7dd8a0745244c5fc9839ce1af4f5efde1d76c7
Yann Bouilhac <yann@bouilhac.net>
2019-09-11 15:16:28 +0200

    Example showing how to use a function defined in an other file in PHP.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php require_once 'add.php'; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>How to work with multiple files in PHP | Simplon Clermont</title>
+    </head>
+    <body>
+        <p>Result of the following addition is calculated and displayed in PHP.</p>
+        <p>5 + 4 = <?php echo add(5, 4); ?></p>
+    </body>
+</html>
