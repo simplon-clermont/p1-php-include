8f5271b2d2f1a7dd76cf63c2faff7aed5979e539
Simplon Clermont <yann+simplon@bouilhac.net>
2019-09-25 10:47:36 +0200

    Add type verification with exceptions.

--- add.php
+++ add.php
@@ -9,5 +9,12 @@
  * @return int
  */
 function add($a, $b) {
+    if (!is_numeric($a)) {
+        throw new Exception("Bad first operand type for add function (expected numeric) ", 1);
+    }
+    if (!is_numeric($a)) {
+        throw new Exception("Bad second operand type for add function (expected numeric) ", 2);
+    }
+
     return intval($a) + intval($b);
 }
